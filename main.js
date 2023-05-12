e51e333f78c14cfbc77174c6aad961ff0bfa0999
jeonghwanJay <nawhgnoejij@naver.com>
2023-05-12 23:31:50 +0900

    Refactoring target-coordinates (performance up!)

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-const vertical = document.querySelector('.vertical');
-const horizontal = document.querySelector('.horizontal');
-const target = document.querySelector('.target');
-const coordinates = document.querySelector('.coordinates');
-document.addEventListener('mousemove', (e) => {
+const vertical = document.querySelector(".vertical");
+const horizontal = document.querySelector(".horizontal");
+const target = document.querySelector(".target");
+const coordinates = document.querySelector(".coordinates");
+
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+  document.addEventListener("mousemove", (e) => {
     const x = e.clientX;
     const y = e.clientY;
-    target.style.left = `${x}px`
-    target.style.top = `${y}px`;
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    coordinates.style.left = `${x}px`;
-    coordinates.style.top = `${y}px`;
-    coordinates.innerHTML = `${x}px, ${y}px`
-})
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    coordinates.style.transform = `translate(${x}px, ${y}px)`;
+    coordinates.innerHTML = `${x}px, ${y}px`;
+  });
+});
